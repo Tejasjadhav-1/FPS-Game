ecf547bbe708887b8c292a3048d9074669b1ee3d
Tejasjadhav-1 <tj21314151@gmail.com>
2026-07-19 23:43:08 +0530

    Added FOV change while sprinting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,21 @@
-using Unity.Mathematics;
 using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float gravity = -9.8f;
-    [SerializeField] float jumpHeight = 2f;
     [SerializeField] float sprintSpeed = 5f;
+    
+    [SerializeField] float jumpHeight = 2f;
+    [SerializeField] float sprintJumpBoost = 3f;
+
+    [SerializeField] Camera playerCamera;
+
+    [SerializeField] float normalFOV = 60f;
+    [SerializeField] float sprintFOV = 75f;
+
+    [SerializeField] float fovSmoothSpeed = 8f;
+
+    [SerializeField] float gravity = -9.8f;
 
     float verticalVelocity;
 
@@ -21,20 +30,25 @@ public class Playermovement : MonoBehaviour
 
     private void Update()
     {
+        
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");     
 
         Vector3 move = transform.forward * vertical + transform.right * horizontal;
         move = move.normalized;
+        bool isSprinting = Input.GetKey(KeyCode.LeftShift) && vertical > 0;
 
         float currentSpeed = moveSpeed;
+        float targetFOV = normalFOV;
 
-        if (Input.GetKey(KeyCode.LeftShift) && vertical > 0)
+        if (isSprinting)
         {
             currentSpeed = sprintSpeed;
-        }       
+            targetFOV = sprintFOV;
+        }
+        playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, targetFOV, fovSmoothSpeed * Time.deltaTime);
 
-        if (controller.isGrounded == true && verticalVelocity < 0)
+        if (controller.isGrounded && verticalVelocity < 0)
         {
             verticalVelocity = -2f;
         }   
@@ -42,9 +56,11 @@ public class Playermovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
         {
             verticalVelocity = Mathf.Sqrt(-2*gravity*jumpHeight);
-            Debug.Log(jumpHeight);
+            if(isSprinting)
+            {
+                verticalVelocity += sprintJumpBoost;
+            }
         }
-
 
         verticalVelocity += gravity * Time.deltaTime;
 
